fd100727fa66fda4f7b664d60dcccd4c8c5dc623
SuperHamzzi <gksrnrrl6gh@naver.com>
2022-12-16 01:53:15 +0900

    12-15 commit

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -11,53 +11,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.board.entity.Board;
 import kr.board.mapper.BoardMapper;
 
 @Controller
-public class BoardController{	// new BoardController();
-	// /boardList.do
+public class BoardController{
+	
 	@Autowired
-	private BoardMapper mapper;
-	// HandlerMapping
+	BoardMapper boardMapper;
+	
+	@RequestMapping("/")
+	public String main() {
+		return "main";
+	}
+	
 	@RequestMapping("/boardList.do")
-	public String boardList(Model model) {
-		List<Board> list=mapper.getLists();
-		model.addAttribute("list", list);
-		return "boardList"; // /WEB-INF/views/boardList.jsp -> forward
-	}	
-	@GetMapping("/boardForm.do")
-	public String boardForm() {
-		return "boardForm"; // /WEB-INF/views/boardForm.jsp -> forward
+	public @ResponseBody List<Board> boardList(){
+		List<Board> list = boardMapper.getLists();
+		return list; //JSON 데이터 형식으로 변환해서 리턴한다.
 	}
-	@PostMapping("/boardInsert.do")
-	public String boardInsert(Board vo) { // title, content, writer => 파라메터수집(Board)
-		mapper.boardInsert(vo); // 등록		
-		return "redirect:/boardList.do"; // redirect
+	
+	@RequestMapping("/boardInsert.do")
+	public @ResponseBody void boardInsert(Board vo) {
+		boardMapper.boardInsert(vo); //등록 성공
+		
 	}
-	@GetMapping("/boardContent.do")
-	public String boardContent(int idx, Model model) { // ?idx=6
-		Board vo=mapper.boardContent(idx);
-		// 조회수 증가
-		mapper.boardCount(idx);
-		model.addAttribute("vo", vo); // ${vo.idx}...
-		return "boardContent"; // boardContent.jsp
-	}
-	@GetMapping("/boardDelete.do/{idx}")
-	public String boardDelete(@PathVariable("idx") int idx) { // ?idx=6
-		mapper.boardDelete(idx); //삭제		
-		return "redirect:/boardList.do";
-	}	
-	@GetMapping("/boardUpdateForm.do/{idx}")
-	public String boardUpdateForm(@PathVariable("idx") int idx, Model model) {
-		Board vo=mapper.boardContent(idx);
-		model.addAttribute("vo", vo);		
-		return "boardUpdate"; // boardUpdate.jsp
-	}
-	@PostMapping("/boardUpdate.do")
-	public String boardUpdate(Board vo) { // idx, title, content
-		mapper.boardUpdate(vo); // 수정		
-		return "redirect:/boardList.do";
-	}
+	
 }
